f91536c953dd8a07ffaed9ad3b22ac03ad33adf4
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-08-02 04:42:38 -0300

    add prettyPrint function and test case

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -73,6 +73,7 @@ class Tree {
         i--;
       }
     }
+    return array;
   }
 }
 
@@ -83,3 +84,20 @@ class Node {
     this.rightChild = null;
   }
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+prettyPrint(tree.root);
